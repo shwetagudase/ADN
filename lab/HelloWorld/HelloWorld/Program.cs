a8f65db2d0f934cc3680dc3d874085eb97ccea93
Shweta Gudase <shwetagudse@gmail.com>
2026-01-23 11:32:22 +0530

    Finalize remaining work

---
+++ lab/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,150 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World !!");
+
+            //Console.WriteLine(3 + 3);
+
+            //Console.Write("hello !!");
+            //Console.Write("This is on the same line..");
+
+            //String name = "Shweta";
+            //Console.WriteLine(name);
+
+            //int num = 12;
+            //Console.WriteLine(num);
+
+            //double num2 = 20.2;
+            //Console.WriteLine(num2);
+
+            //char var = 'B';
+            //Console.WriteLine(var);
+
+            //bool Bool_val = true;
+            //Console.WriteLine(Bool_val);
+
+            //String Text = "Any name";
+            //Console.WriteLine(Text);
+            //int x = 10;
+            //int y = 30;
+            //Console.WriteLine(x + y);
+
+            //Console.WriteLine("Enter Name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Your name : " + name);
+
+            //Console.WriteLine("Enter your age :");
+            //int age = Console.ReadLine();
+            //Console.WriteLine("Your age : " + age);
+
+            //int a = 5;
+            //int b = 3;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a % b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(a++);
+            //Console.WriteLine(b--);
+            //Math.Max(10, 5);
+            //Console.WriteLine(Math.Max(10, 5));
+            //Console.WriteLine(Math.Min(10, 5));
+            //Console.WriteLine(Math.Sqrt(64));
+            //Console.WriteLine(Math.Abs(-10));
+            //Console.WriteLine(Math.Round(9.99));
+
+
+
+            //String str1 = "Shweta";
+            //Console.WriteLine(str1);
+            //Console.WriteLine("Hello my name is" + str1);
+            //Console.WriteLine(str1.Length);
+            //Console.WriteLine(str1.ToUpper());
+            //Console.WriteLine(str1.ToLower());
+            //string str1 = "shweta";
+            //string str2 = "Gudase";
+            //Console.WriteLine(str1 + str2);
+            //string name = string.Concat(str1, str2);
+            //Console.WriteLine(name);
+            //int a = 10;
+            //int b = 12;
+            //Console.WriteLine(a + b);
+            //string str1 = "shweta";
+            //string str2 = "Gudase";
+            //string name = $"My name is:{str1} {str2}";
+            //Console.WriteLine(name);
+            //Console.WriteLine(str1[0]);
+            //Console.WriteLine(str1.IndexOf("e"));
+            //int chrPos=str1.IndexOf("e");
+            //Console.WriteLine(str1.Substring(chrPos));
+
+            //int age = 21;
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("You can Vote!!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You can not Vote!!");
+            //}
+
+            //int time = 8;
+            //if (time < 10)
+            //{
+            //    Console.WriteLine("Good Morning ");
+            //}
+            //else if (time < 18)
+            //{
+            //    Console.WriteLine("Good Afternoon");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Good evening!!");
+            //}
+
+            //int time = 10;
+            //string result=(time < 18 ? "Good Morning" : "Good Afternoon") ;
+            //Console.WriteLine(result);
+
+            //int day = 4;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("day 1..");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("day2...");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("day 3...");
+            //        break;
+            //    case 4 :
+            //        Console.WriteLine("day 4....");
+            //        break;
+            //    default:
+            //        Console.WriteLine(" day is not availabel");
+            //        break;
+
+            //}
+            //int i = 0;
+            //while (i < 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i <= 5);
+        }
+    }
+}
